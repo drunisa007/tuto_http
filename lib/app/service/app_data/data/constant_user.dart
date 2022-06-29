9cc28e044fd78007cb9b7f9121026962c7968931
drunisa007 <newarunkatwal@gmail.com>
2022-06-29 12:32:46 +0630

    Createing Dummy User List to show in Design

---
+++ lib/app/service/app_data/data/constant_user.dart
@@ -0,0 +1,63 @@
+import 'package:tuto_http/app/service/vos/user_vo.dart';
+
+class ConstantUser {
+  getAllUser() {
+    List<UserVo> dummyUserList = [
+      UserVo(
+          id: "60d0fe4f5311236168a109fd",
+          title: "mr",
+          firstName: "Dylan",
+          lastName: "Vasquez",
+          picture: "https://randomuser.me/api/portraits/med/men/66.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a109fe",
+          title: "mrs",
+          firstName: "Josefina",
+          lastName: "Calvo",
+          picture: "https://randomuser.me/api/portraits/med/women/3.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a10a00",
+          title: "mrs",
+          firstName: "Els",
+          lastName: "Ijsseldijk",
+          picture: "https://randomuser.me/api/portraits/med/women/75.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a10a01",
+          title: "mr",
+          firstName: "Jesus",
+          lastName: "Riley",
+          picture: "https://randomuser.me/api/portraits/med/men/45.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a10a02",
+          title: "mr",
+          firstName: "Cristobal",
+          lastName: "Soler",
+          picture: "https://randomuser.me/api/portraits/med/men/31.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a10a03",
+          title: "mr",
+          firstName: "Andri",
+          lastName: "Leclerc",
+          picture: "https://randomuser.me/api/portraits/med/men/57.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a10a04",
+          title: "mr",
+          firstName: "Konsta",
+          lastName: "Manninen",
+          picture: "https://randomuser.me/api/portraits/med/men/24.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a10a05",
+          title: "mr",
+          firstName: "Ane",
+          lastName: "Frafjord",
+          picture: "https://randomuser.me/api/portraits/med/women/54.jpg"),
+            UserVo(
+          id: "60d0fe4f5311236168a10a06",
+          title: "mr",
+          firstName: "OlaÃ­",
+          lastName: "Gomes",
+          picture: "https://randomuser.me/api/portraits/med/women/85.jpg"),
+
+    ];
+  }
+}
